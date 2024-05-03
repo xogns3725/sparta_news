acb958a852e87790b119d084fc7a267950798f08
BeomJung <kbeom0124@naver.com>
2024-05-03 12:16:52 +0900

    made signup feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import  UserSerializer
 
-# Create your views here.
+
+class UserAPIView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
--- sparta_news/settings.py
+++ sparta_news/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     # local
     'accounts',
     'articles',
-    
+
 ]
 
 MIDDLEWARE = [
