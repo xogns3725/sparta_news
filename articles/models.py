2778822d23c3c184013f81b418dcaadbfc1d4997
BeomJung <kbeom0124@naver.com>
2024-05-03 11:25:17 +0900

    초기 세팅

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,19 @@
+from tkinter import CASCADE
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    url = models.TextField()
+    author = models.ForeignKey(User, on_delete=CASCADE, related_name='article_author')
+    article_likes = models.ManyToManyField(related_name='article_likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comments(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=CASCADE, related_name='comment_author')
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment_likes = models.ManyToManyField(related_name='comment_likes')
