0b542811af81fc8db25d3af36bb4cdd1b97c6ee3
hunhee92 <rohhelove@daum.net>
2024-05-03 14:25:19 +0900

    삭제완료

--- articles/urls.py
+++ articles/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.ArticleListAPIView.as_view(), name="article_list"),
+    path("<int:pk>/", views.ArticleDetailAPIView.as_view(),
+         name="article_detail")
 ]
--- articles/views.py
+++ articles/views.py
@@ -21,3 +21,23 @@ class ArticleListAPIView(APIView):
             # 작성자 로그인한 유저 author = request.user
             serializer.save(author=User.objects.get(id=1))
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ArticleDetailAPIView(APIView):
+    def get(self, request, pk):
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleSrializer(article)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        article = get_object_or_404(Article, pk=pk)
+        article.delete()
+
+    #   user =request.user.id
+    #   u_user= article.article_author.id
+    #   if user ==u_user : 글쓴이와 로그인한 사용자 검증
+        return Response(status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+
+        return Response({})
