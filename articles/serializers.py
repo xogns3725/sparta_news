a29b02701175f66c9bf176bd3d5ac3f149c10206
xogns3725 <xogns3725@naver.com>
2024-05-07 10:25:59 +0900

    article_likes func

--- articles/serializers.py
+++ articles/serializers.py
@@ -3,12 +3,16 @@ from rest_framework import serializers
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    
     class Meta:
         model = Article
-        fields = ['title', 'content', 'url',]
+        fields = ['title', 'content', 'url', 'likes_count',]
+    
+    def get_likes_count(self, obj):
+        return obj.article_likes.count()
         
-class ArticleDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ['title', 'content', 'url',]
+class ArticleDetailSerializer(ArticleSerializer):
+    # comment 추가해야합니다.
+    pass
         
